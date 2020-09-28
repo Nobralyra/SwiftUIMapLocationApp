49560e6ec30570e8cdeef31953b38dbe6e1d781e
Nobralyra <signees96@gmail.com>
2020-09-28 09:11:05 +0200

    Initial Commit

---
+++ SwiftUIMapLocationApp/SwiftUIMapLocationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapLocationAppApp.swift
+//  SwiftUIMapLocationApp
+//
+//  Created by admin on 28/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapLocationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
