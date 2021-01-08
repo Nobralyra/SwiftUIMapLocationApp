f4a213f032965e4014405281468ddd575300d8ec
Nobralyra <signees96@gmail.com>
2021-01-08 18:07:11 +0100

    First commit

--- SwiftUIMapLocationApp/ContentView.swift
+++ SwiftUIMapLocationApp/ContentView.swift
@@ -6,11 +6,16 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct ContentView: View
+{
+    
+    var body: some View
+    {
+        // $ er en binidng, som sørger for at update @State region, mens man bruger kortet
+        //Map(coordinateRegion: $region)
+        Home()
     }
 }
 
@@ -18,4 +23,97 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+// First with Full Accuracy
+
+struct Home: View
+{
+    // @State to directs the center of the map and how much of the area around is visible around the map (zoom level)
+    // MKCoordinateRegion(center:span:) - hvor centrum er og hvor meget der kan ses rundt om det
+    // For center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275)
+    // For span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+   
+    @State var tracking : MapUserTrackingMode = .follow
+    
+    @State var manager = CLLocationManager()
+    
+    @StateObject var managerDelegate = locationDelegate()
+    
+    var body: some View
+    {
+        VStack
+        {
+            //Map(coordinateRegion: $region, interactionModes: .all, showsUserLocation: true, userTrackingMode: $tracking)
+            Map(coordinateRegion: $region, interactionModes: .all, showsUserLocation: true, userTrackingMode: $tracking, annotationItems: managerDelegate.pins)
+            {
+                pin in
+                MapPin(coordinate: pin.location.coordinate, tint: .red)
+            }
+        }
+        .onAppear
+        {
+            manager.delegate = managerDelegate
+        }
+    }
+}
+
+
+// Location manager delegate
+// Now when precise location is not turned on
+class locationDelegate : NSObject, ObservableObject, CLLocationManagerDelegate
+{
+    @Published var pins : [Pin] = []
+    // Checking authorization status
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager)
+    {
+        // we're going to use only when in use Key only
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse
+        {
+            print("Authorized..")
+            
+            // setting reduced accuracy to false and updating locations
+            
+            // checking whether precise location is turned on
+            if manager.accuracyAuthorization != .fullAccuracy
+            {
+                print("reduced accuracy")
+                
+                // requesting temporary accuracy
+                manager.requestTemporaryFullAccuracyAuthorization(withPurposeKey: "Location")
+                {
+                    (error) in
+                    if error != nil
+                    {
+                        print(error!)
+                        return
+                    }
+                }
+            }
+            manager.startUpdatingLocation()
+        }
+        else
+        {
+            print("No authorized")
+            // requesting acces
+            
+            manager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        pins.append(Pin(location: locations.last!))
+    }
+}
+
+//Map pins for updates
+
+struct Pin: Identifiable
+{
+    var id = UUID().uuidString
+    var location: CLLocation
 }
